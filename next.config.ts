fe7a8ddaa602af08e478ed66ea8b7375d42f5a53
fr13dman <haxx00rr@gmail.com>
2025-05-15 22:49:19 -0400

    fixed netlify and next.config to reflect correct destDir

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     /* config options here */
-    output: 'standalone',
-    distDir: 'frontend/job-app-website/.next',
+    distDir: '.next',
     basePath: '',
 }
 
